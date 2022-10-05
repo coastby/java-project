965a1f33d30deb870793fb0276d00c8643bc944d
JO YEJI <whdpwl2@gmail.com>
2022-10-05 10:47:53 +0900

    calculator 생성자 생성

--- java-project/src/Oct05/CalculatorMain.java
+++ java-project/src/Oct05/CalculatorMain.java
@@ -1,15 +1,12 @@
 package Oct05;
 public class CalculatorMain {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 50;
+        Calculator calculator = new Calculator(10, 50);
 
-        Calculator calculator = new Calculator();
-
-        calculator.plus(a, b);
-        calculator.minus(a, b);
-        calculator.multiple(a, b);
-        calculator.devide(a, b);
+        calculator.plus();
+        calculator.minus();
+        calculator.multiple();
+        calculator.devide();
 
     }
 }
