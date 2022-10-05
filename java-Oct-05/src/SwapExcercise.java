974a8db6f0a93f130e5bf1bc7774b66f49ce9713
JO YEJI <whdpwl2@gmail.com>
2022-10-05 09:18:18 +0900

    리포지토리 생성 후 Swap 업로드

---
+++ java-Oct-05/src/SwapExcercise.java
@@ -0,0 +1,13 @@
+public class SwapExcercise {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 50;
+
+        int temp = a;
+
+        a = b;
+        b = temp;
+        System.out.println("a:" + a + " b:" + b);
+
+    }
+}
