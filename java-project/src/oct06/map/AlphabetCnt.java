24135da6d2ff35b34ea96b34c7c3e481902245b4
JO YEJI <whdpwl2@gmail.com>
2022-10-06 13:46:27 +0900

    alphabet counting

--- java-project/src/oct06/map/AlphabetCnt.java
+++ java-project/src/oct06/map/AlphabetCnt.java
@@ -1,25 +1,34 @@
 package oct06.map;
 
+import java.util.HashMap;
+
 public class AlphabetCnt {
     //해당 캐릭터가 알파벳이면 true, 아니면 false 반환
     static boolean isAlphabet(char ch) {
-        boolean result = true;
-        if (ch < 65 || (ch > 90 && ch < 97) || ch >122) {
-            result = false;
+        boolean result = false;
+        //ch를 아스키로 바꾸지 않아도 바로 비교 가능
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+            result = true;
         }
         return result;
     }
 
     public static void main(String[] args) {
-        String s1 = "aaabbbcc//dd-efft-gg";
+        String s1 = "aAAaab...bbcc//dd-efft-gg".toUpperCase();
+        HashMap<Character, Integer> alphabetMap = new HashMap<>();
 
-        //String을 한 글자씩 출력
+        //알파벳 키값 세팅
+        for (char c = 'A'; c <= 'Z'; c++) {
+            alphabetMap.put(c, 0);
+        }
+        //하나씩 찾아서 카운팅
         for (int i = 0; i < s1.length(); i++) {
             char ch = s1.charAt(i);
             if (isAlphabet(ch)) {
-                System.out.println(ch);
+                alphabetMap.put(ch, alphabetMap.get(ch)+1);
             }
         }
+        System.out.println(alphabetMap);
 
     }
 }
