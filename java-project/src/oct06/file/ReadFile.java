2620ff7bbe4c112bd99ca53c65b60dab5fd2e27f
JO YEJI <whdpwl2@gmail.com>
2022-10-06 14:54:52 +0900

    read n bytes of file

--- java-project/src/oct06/file/ReadFile.java
+++ java-project/src/oct06/file/ReadFile.java
@@ -16,16 +16,28 @@ public class ReadFile {
     }
 
     public char readOneByte () throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(filename), 16 * 1024);
+        BufferedReader br = new BufferedReader(new FileReader(filename));
         return (char)br.read();
     }
 
+    public String readTwoByte () throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+        String result = Character.toString(br.read()) + Character.toString(br.read());
+        return result;
+    }
+
+    /**
+     * param int num
+     * return String
+     * throws IOException
+     * num의 수만큼 글자를 읽어서 문자열로 반환하는 함수
+     */
     public String readMultiByte (int num) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(filename), 16 * 1024);
+        BufferedReader br = new BufferedReader(new FileReader(filename));
         String result = "";
         //num만큼의 글자를 문자열에 붙여넣기
         for (int i = 0; i < num; i++) {
-            result += String.valueOf((char)br.read());
+            result += String.valueOf((char) br.read());
         }
         return result;
     }
--- java-project/src/oct06/file/ReadFileMain.java
+++ java-project/src/oct06/file/ReadFileMain.java
@@ -5,12 +5,18 @@ import java.io.IOException;
 public class ReadFileMain {
     public static void main(String[] args) throws IOException {
         ReadFile rf = new ReadFile("a_file.txt");
-        //1바이트 읽기
+        //1글자 읽기
         char r1 = rf.readOneByte();
         System.out.println(r1);
 
-        //매개인자 수만큼 문자열로 읽어오기
+        //2글자 읽기
+        String r3 = rf.readTwoByte();
+        System.out.println(r3);
+
+        //매개인자 수만큼 문자열로 읽어오수
         String r2 = rf.readMultiByte(5);
         System.out.println(r2);
+
+
     }
 }
