6d1de0cd15bbe1716be271a8a0c2b370e651ecb5
JO YEJI <whdpwl2@gmail.com>
2022-10-06 14:42:11 +0900

    글자 n개 가져오기

--- java-project/src/oct06/file/ReadFile.java
+++ java-project/src/oct06/file/ReadFile.java
@@ -20,5 +20,13 @@ public class ReadFile {
         return (char)br.read();
     }
 
-
+    public String readMultiByte (int num) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(filename), 16 * 1024);
+        String result = "";
+        //num만큼의 글자를 문자열에 붙여넣기
+        for (int i = 0; i < num; i++) {
+            result += String.valueOf((char)br.read());
+        }
+        return result;
+    }
 }
--- java-project/src/oct06/file/ReadFileMain.java
+++ java-project/src/oct06/file/ReadFileMain.java
@@ -5,7 +5,12 @@ import java.io.IOException;
 public class ReadFileMain {
     public static void main(String[] args) throws IOException {
         ReadFile rf = new ReadFile("a_file.txt");
-        char c1 = rf.readOneByte();
-        System.out.println(c1);
+        //1바이트 읽기
+        char r1 = rf.readOneByte();
+        System.out.println(r1);
+
+        //매개인자 수만큼 문자열로 읽어오기
+        String r2 = rf.readMultiByte(5);
+        System.out.println(r2);
     }
 }
