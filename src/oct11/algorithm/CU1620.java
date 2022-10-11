1a42d0eec75d5bf411b3f059e3a779605b98c2b4
JO YEJI <whdpwl2@gmail.com>
2022-10-11 17:29:07 +0900

    codeup 1620 자리수의 합

---
+++ src/oct11/algorithm/CU1620.java
@@ -0,0 +1,19 @@
+package oct11.algorithm;
+
+import java.util.Scanner;
+
+public class CU1620 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while (n >= 10) {
+            int sum = 0;
+            while(n > 0) {
+                sum += n%10;
+                n = n/10;
+            }
+            n = sum;
+        }
+        System.out.println(n);
+    }
+}
