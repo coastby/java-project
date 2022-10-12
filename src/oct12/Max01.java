10aef583c506506cd85ea28ef128ffc432411b63
JO YEJI <whdpwl2@gmail.com>
2022-10-12 10:29:14 +0900

    getMax() : 최대값인덱스와 최대값을 반환하도록 리팩토링

--- src/oct12/Max01.java
+++ src/oct12/Max01.java
@@ -1,22 +1,30 @@
 package oct12;
 
 public class Max01 {
-    public int getMax(int[] arr) {
+    /**
+     * param  : int[] arr
+     * return : int[] [최대값인덱스, 최대값]
+     */
+    public int[] getMax(int[] arr) {
+        int maxIdx = 0;
         int maxVal = arr[0];
         for (int i = 0; i < arr.length; i++) {
-            maxVal = (arr[i] > maxVal)? arr[i] : maxVal;
+            if (arr[i] > maxVal) {
+                maxVal = arr[i];
+                maxIdx = i;
+            }
         }
-        return maxVal;
+        return new int[] {maxIdx, maxVal};
     }
 
     public static void main(String[] args) {
         int[] arr = {3, 29, 38, 12, 57, 74, 40, 85, 61};
         Max01 m1 = new Max01();
-        int result = m1.getMax(arr);
-        if (result == 85) {
+        int[] result = m1.getMax(arr);
+        if (result[1] == 85 && result[0] == 7) {
             System.out.println("Pass");
         } else {
-            System.out.println("Fail");
+            System.out.println("Fail" + result[0]);
         }
     }
 }
