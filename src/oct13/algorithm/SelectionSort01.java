648324a479668d22b846ffe70c6d2cf1b3d03fe3
JO YEJI <whdpwl2@gmail.com>
2022-10-13 09:24:02 +0900

    selectionSort로 정정

--- src/oct13/algorithm/SelectionSort01.java
+++ src/oct13/algorithm/SelectionSort01.java
@@ -3,8 +3,8 @@ package oct13.algorithm;
 import java.util.Arrays;
 
 //버블정렬 : 자기 자리 기준 오른쪽에 있는 수들과 비교를 한다.
-public class BubbleSort01 {
-    public int[] bubbleSort(int[] arr) {
+public class SelectionSort01 {
+    public int[] selectionSort(int[] arr) {
         for (int i = 0; i < (arr.length-1); i++) {
             //더 작은 수를 넣을 변수
             int tmp;
@@ -20,10 +20,10 @@ public class BubbleSort01 {
     }
 
     public static void main(String[] args) {
-        BubbleSort01 bs = new BubbleSort01();
+        SelectionSort01 bs = new SelectionSort01();
         int[] arr = {3, 36, 23, 5, 8};
 
-        arr = bs.bubbleSort(arr);
+        arr = bs.selectionSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
