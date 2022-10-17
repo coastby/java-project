3e8b3ee31a57484b873a32d421778bef0d5480ce
JO YEJI <whdpwl2@gmail.com>
2022-10-17 09:19:50 +0900

    정수 n을 받아서 n단의 직각삼각형 그리는 메서드

--- src/oct17/algorithm/RightTriangle.java
+++ src/oct17/algorithm/RightTriangle.java
@@ -1,11 +1,15 @@
 package oct17.algorithm;
 
 public class RightTriangle {
-    public static void main(String[] args) {
-        for (int i = 0; i < 4; i++) {
+    public void makeRightTriangle (int n) {
+        for (int i = 0; i < n; i++) {
             for (int j= 0; j <= i; j++)
                 System.out.print("* ");
             System.out.println();
         }
+    }
+    public static void main(String[] args) {
+        RightTriangle rightTriangle = new RightTriangle();
+        rightTriangle.makeRightTriangle(10);
     }
 }
