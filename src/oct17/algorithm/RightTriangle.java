4d1f98de94b4711f8d2669eeb84d5518263a7aff
JO YEJI <whdpwl2@gmail.com>
2022-10-17 09:38:29 +0900

    직각삼각형 찍는 메서드 1. 생성자로 어떤 character로 찍을 지 넘겨줌. default는 '*' 2. 메서드 실행 시 정수n을 파라미터로 넘겨주고 n단만큼 출력

--- src/oct17/algorithm/RightTriangle.java
+++ src/oct17/algorithm/RightTriangle.java
@@ -1,15 +1,23 @@
 package oct17.algorithm;
 
 public class RightTriangle {
+    char letter;
+    public RightTriangle() {
+        this.letter = '#';
+    }
+    public RightTriangle(char letter) {
+        this.letter = letter;
+    }
+
     public void makeRightTriangle (int n) {
         for (int i = 0; i < n; i++) {
             for (int j= 0; j <= i; j++)
-                System.out.print("* ");
+                System.out.printf("%c ", letter);
             System.out.println();
         }
     }
     public static void main(String[] args) {
-        RightTriangle rightTriangle = new RightTriangle();
+        RightTriangle rightTriangle = new RightTriangle('d');
         rightTriangle.makeRightTriangle(10);
     }
 }
