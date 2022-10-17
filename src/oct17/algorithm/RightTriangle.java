caba316b9b1d1d3dd845cf4b5ae78f22d7ae5da9
JO YEJI <whdpwl2@gmail.com>
2022-10-17 09:17:57 +0900

    직각삼각형 별찍기

--- src/oct17/algorithm/RightTriangle.java
+++ src/oct17/algorithm/RightTriangle.java
@@ -2,9 +2,10 @@ package oct17.algorithm;
 
 public class RightTriangle {
     public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
+        for (int i = 0; i < 4; i++) {
+            for (int j= 0; j <= i; j++)
+                System.out.print("* ");
+            System.out.println();
+        }
     }
 }
