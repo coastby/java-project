cde8f68392fb7591fcc5374dad084d19370a4c4f
JO YEJI <whdpwl2@gmail.com>
2022-10-18 09:18:26 +0900

    생성자 추가 생성자로 넘겨준 글자를 이용해 사각형을 출력한다.

--- src/oct18/algorithm/SqureStar.java
+++ src/oct18/algorithm/SqureStar.java
@@ -1,23 +1,32 @@
 package oct18.algorithm;
 
 public class SqureStar {
+    String letter;
+    public SqureStar() {
+        this.letter = "*";
+    }
+    public SqureStar(String letter) {
+        this.letter = letter;
+    }
+
     public void printSqure (int size) {
         for (int i = 0; i < size; i++) {
-            System.out.print("*".repeat(size)+"\n");
+            System.out.print(letter.repeat(size)+"\n");
         }
     }
 
     public void printRectangle(int width, int height) {
         for (int i = 0; i < height; i++) {
-            System.out.print("*".repeat(width)+"\n");
+            System.out.print(letter.repeat(width)+"\n");
         }
     }
 
     public static void main(String[] args) {
         int size = 10;
         SqureStar ss = new SqureStar();
+        SqureStar sss = new SqureStar("s");
         ss.printSqure(size);
         System.out.println();
-        ss.printRectangle(5, 3);
+        sss.printRectangle(5, 3);
     }
 }
